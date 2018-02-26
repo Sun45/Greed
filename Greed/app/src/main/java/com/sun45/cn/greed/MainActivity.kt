6382fa20e79ecdbbcccd08b1d1ed9ceb896d9ed2
Sun45 <135514124@qq.com>
2018-02-26 15:20:28 +0800

    first build

---
+++ Greed/app/src/main/java/com/sun45/cn/greed/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sun45.cn.greed
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
