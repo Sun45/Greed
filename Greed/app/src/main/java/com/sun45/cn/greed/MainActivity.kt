8656eccb71837dc8499a8f239d43ad38606c9de1
Sun45 <135514124@qq.com>
2018-02-26 15:32:37 +0800

    test commit

--- Greed/app/src/main/java/com/sun45/cn/greed/MainActivity.kt
+++ Greed/app/src/main/java/com/sun45/cn/greed/MainActivity.kt
@@ -3,6 +3,9 @@ package com.sun45.cn.greed
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * 首页
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ Greed/app/src/main/java/com/sun45/cn/greed/Test.kt
@@ -0,0 +1,8 @@
+package com.sun45.cn.greed
+
+/**
+ * Created by Sun45 on 2018/2/26.
+ * 测试
+ */
+class Test {
+}
