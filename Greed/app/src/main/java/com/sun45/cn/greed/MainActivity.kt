54fc42e09b588b721bb468a1b22cf57b34dbef38
Sun45 <135514124@qq.com>
2018-02-28 13:58:56 +0800

    TriangleArray

--- Greed/app/src/main/java/com/sun45/cn/greed/MainActivity.kt
+++ Greed/app/src/main/java/com/sun45/cn/greed/MainActivity.kt
@@ -4,8 +4,10 @@ import android.app.ActivityManager
 import android.opengl.GLSurfaceView
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.SurfaceView
 import android.widget.TextView
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
@@ -14,6 +16,7 @@ import javax.microedition.khronos.opengles.GL10
  * 首页
  */
 class MainActivity : AppCompatActivity() {
+    private val mTriangleArray = floatArrayOf(0f, 1f, 0f, -1f, -1f, 0f, 1f, -1f, 0f)
     private lateinit var text: TextView
     private lateinit var surface: GLSurfaceView
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,9 +37,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     inner class GLRenderer : GLSurfaceView.Renderer {
+        private lateinit var mTriangleBuffer: FloatBuffer
 
-        //        override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
-//        }
+        fun GLRenderer() {
+            //先初始化buffer，数组的长度*4，因为一个float占4个字节
+            val bb = ByteBuffer.allocateDirect(mTriangleArray.size * 4)
+            //以本机字节顺序来修改此缓冲区的字节顺序
+            bb.order(ByteOrder.nativeOrder())
+            mTriangleBuffer = bb.asFloatBuffer()
+            //将给定float[]数据从当前位置开始，依次写入此缓冲区
+            mTriangleBuffer.put(mTriangleArray)
+            //设置此缓冲区的位置。如果标记已定义并且大于新的位置，则要丢弃该标记。
+            mTriangleBuffer.position(0)
+        }
+
         override fun onSurfaceCreated(gl: GL10, config: EGLConfig) {
             gl.glClearColor(1f, 0f, 0f, 0f)
         }
