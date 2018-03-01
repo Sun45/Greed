b25588220b861c969b6722d9f091fcb94c3f4b7c
Sun45 <135514124@qq.com>
2018-03-01 18:02:38 +0800

    TriangleArray

--- Greed/app/src/main/java/com/sun45/cn/greed/MainActivity.kt
+++ Greed/app/src/main/java/com/sun45/cn/greed/MainActivity.kt
@@ -37,9 +37,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     inner class GLRenderer : GLSurfaceView.Renderer {
-        private lateinit var mTriangleBuffer: FloatBuffer
+        private val mTriangleArray = floatArrayOf(0f, 1f, 0f, -1f, -1f, 0f, 1f, -1f, 0f)
+        //三角形各顶点颜色(三个顶点)
+        private val mColor = floatArrayOf(1f, 1f, 0f, 1f, 0f, 1f, 1f, 1f, 1f, 0f, 1f, 1f)
+        private val mTriangleBuffer: FloatBuffer
+        private val mColorBuffer: FloatBuffer
 
-        fun GLRenderer() {
+        init {
+            //点相关
             //先初始化buffer，数组的长度*4，因为一个float占4个字节
             val bb = ByteBuffer.allocateDirect(mTriangleArray.size * 4)
             //以本机字节顺序来修改此缓冲区的字节顺序
@@ -49,18 +54,70 @@ class MainActivity : AppCompatActivity() {
             mTriangleBuffer.put(mTriangleArray)
             //设置此缓冲区的位置。如果标记已定义并且大于新的位置，则要丢弃该标记。
             mTriangleBuffer.position(0)
-        }
 
-        override fun onSurfaceCreated(gl: GL10, config: EGLConfig) {
-            gl.glClearColor(1f, 0f, 0f, 0f)
-        }
 
-        override fun onSurfaceChanged(gl: GL10, width: Int, height: Int) {
-            gl.glViewport(0, 0, width, height)
+            //颜色相关
+            val bb2 = ByteBuffer.allocateDirect(mColor.size * 4)
+            bb2.order(ByteOrder.nativeOrder())
+            mColorBuffer = bb2.asFloatBuffer()
+            mColorBuffer.put(mColor)
+            mColorBuffer.position(0)
         }
 
         override fun onDrawFrame(gl: GL10) {
-            gl.glClear(GL10.GL_COLOR_BUFFER_BIT)
+            // 清除屏幕和深度缓存
+            gl.glClear(GL10.GL_COLOR_BUFFER_BIT or GL10.GL_DEPTH_BUFFER_BIT)
+            // 重置当前的模型观察矩阵
+            gl.glLoadIdentity()
+
+            // 允许设置顶点
+            //GL10.GL_VERTEX_ARRAY顶点数组
+            gl.glEnableClientState(GL10.GL_VERTEX_ARRAY)
+            // 允许设置颜色
+            //GL10.GL_COLOR_ARRAY颜色数组
+            gl.glEnableClientState(GL10.GL_COLOR_ARRAY)
+
+            //将三角形在z轴上移动
+            gl.glTranslatef(0f, 0.0f, -2.0f)
+
+            // 设置三角形
+            gl.glVertexPointer(3, GL10.GL_FLOAT, 0, mTriangleBuffer)
+            // 设置三角形颜色
+            gl.glColorPointer(4, GL10.GL_FLOAT, 0, mColorBuffer)
+            // 绘制三角形
+            gl.glDrawArrays(GL10.GL_TRIANGLES, 0, 3)
+
+
+            // 取消颜色设置
+            gl.glDisableClientState(GL10.GL_COLOR_ARRAY)
+            // 取消顶点设置
+            gl.glDisableClientState(GL10.GL_VERTEX_ARRAY)
+
+            //绘制结束
+            gl.glFinish()
+
+        }
+
+        override fun onSurfaceChanged(gl: GL10, width: Int, height: Int) {
+            val ratio = width.toFloat() / height
+            // 设置OpenGL场景的大小,(0,0)表示窗口内部视口的左下角，(w,h)指定了视口的大小
+            gl.glViewport(0, 0, width, height)
+            // 设置投影矩阵
+            gl.glMatrixMode(GL10.GL_PROJECTION)
+            // 重置投影矩阵
+            gl.glLoadIdentity()
+            // 设置视口的大小
+            gl.glFrustumf(-ratio, ratio, -1f, 1f, 1f, 10f)
+            //以下两句声明，以后所有的变换都是针对模型(即我们绘制的图形)
+            gl.glMatrixMode(GL10.GL_MODELVIEW)
+            gl.glLoadIdentity()
+
+        }
+
+        override fun onSurfaceCreated(gl: GL10, config: EGLConfig) {
+            // 设置白色为清屏
+            gl.glClearColor(1f, 1f, 1f, 1f)
+
         }
     }
 
